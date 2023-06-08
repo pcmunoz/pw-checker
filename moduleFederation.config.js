ed4c856f487102163ae5a6e015ec94701db531ba
Patrick Ceasar Munoz <munozpatrickceasar@gmail.com>
2023-06-08 17:48:54 +0800

    Add MFE config

---
+++ moduleFederation.config.js
@@ -0,0 +1,23 @@
+const { dependencies } = require('./package.json');
+
+module.exports = {
+  name: 'remote',
+  filename: 'remoteEntry.js',
+  exposes: {
+    './index': './src/index.js',
+  },
+  remotes: {},
+  shared: {
+    ...dependencies,
+    react: {
+      singleton: true,
+      import: 'react',
+      shareScope: 'default',
+      requiredVersion: dependencies.react,
+    },
+    'react-dom': {
+      singleton: true,
+      requiredVersion: dependencies['react-dom'],
+    },
+  },
+};
